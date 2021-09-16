f80f27d4d29d4fac46980086054feb18de56862c
Bacon_Huang <bacon_huang@migocorp.com>
2021-09-16 21:14:52 +0800

    fix

--- finrl/train.py
+++ finrl/train.py
@@ -216,7 +216,7 @@ def train_crypto_trading(config_suffix):
 
         print("==============Model Training===========")
 
-        for model_type in ['ddpg']:
+        for model_type in ['sac']:
             print(f"=============={model_type}===========")
             # if LAST_TRADE_DATE == '':
             model = agent.get_model(model_type)
