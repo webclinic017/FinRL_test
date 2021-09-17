71b948eab92dbefb9c178937daef539d99e52753
Bacon_Huang <bacon_huang@migocorp.com>
2021-09-17 09:46:47 +0800

    fix

--- finrl/apps/config.py
+++ finrl/apps/config.py
@@ -50,7 +50,7 @@ A2C_PARAMS = {"n_steps": 5, "ent_coef": 0.01, "learning_rate": 0.0007}
 PPO_PARAMS = {
     "n_steps": 2048,
     "ent_coef": 0.01,
-    "learning_rate": 0.00025,
+    "learning_rate": 0.001,
     "batch_size": 64,
 }
 DDPG_PARAMS = {"batch_size": 128, "buffer_size": 50000, "learning_rate": 0.001}
